238d769056db37afb965a8a02ee8702dcdd5c04b
Christina Elena Bornberg <christina.bornberg@outlook.com>
2017-12-06 10:22:25 +0100

    gstreamer install

--- GUI/src/gstreamer.c
+++ GUI/src/gstreamer.c
@@ -1,5 +1,5 @@
 // from: https://gstreamer.freedesktop.org/documentation/tutorials/basic/streaming.html
-
+// dnf install gstreamer1-devel gstreamer1-plugins-base-tools gstreamer1-devel-docs gstreamer1-plugins-base-devel gstreamer1-plugins-base-devel-docs gstreamer1-plugins-good gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-ugly-devel-docs  gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free-devel gstreamer1-plugins-bad-free-extras
 #include <gst/gst.h>
 #include <string.h>
 
