0b79d1746e90b724c5465ae20a3d0b7454d828ca
LucasUllrich <lucas.ullrich@aon.at>
2017-12-21 14:10:08 +0100

    Add debugging and sniffer file

---
+++ XMC/template.c
@@ -0,0 +1,37 @@
+/**
+ * Project: XMCarL
+ *
+ *  Description:
+ *    - XMC with Sensor:
+ *      The XMC is connetced to a accelerometer sensor via I2C, reads the data
+ *      from it and forwards it to a PC via UART to a GUI.
+ *
+ *    - PC:
+ *      The PC is used to graphically display the current position of the sensor
+ *      and display a video feed of a GoPro mounted on a RC car.
+ *      Furthermore it is connected to both of the XMC's to act as a
+ *      transmission pipeline.
+ *
+ *    - XMC on RC car:
+ *      The XMC is directly connetced to the controls of the RC car, and via
+ *      Bluetooth to the PC. The from the PC forwarded Date is used to control
+ *      the car.
+ *
+ * Author:
+ *
+ * Date of creation:
+ *
+ * Filedescription:
+ *
+ *
+ */
+
+
+/**
+ * Included Files
+ */
+
+
+/**
+ * 
+ */
