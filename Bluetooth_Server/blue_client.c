a18d07a40ee05482745f6c349df242090c85bbe6
Chrisslybaer <prinzessin@localhost.localdomain>
2018-01-05 10:21:55 +0100

    hope it's not a merge

---
+++ Bluetooth_Server/blue_client.c
@@ -0,0 +1,37 @@
+
+#include <unistd.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/l2cap.h>
+
+int main(int argc, char **argv)
+{
+    struct sockaddr_l2 addr = { 0 };
+    int s, status;
+    char *message = "hello!";
+    char dest[18] = "00:1B:35:88:0C:81";
+
+    // allocate a socket
+    s = socket(AF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
+
+    // set the connection parameters (who to connect to)
+    addr.l2_family = AF_BLUETOOTH;
+    addr.l2_psm = htobs(0x1001);
+    str2ba( dest, &addr.l2_bdaddr );
+
+    printf("thats my socket %d", s);
+    // connect to server
+    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+
+    // send a message
+    if( status == 0 ) {
+        status = write(s, "hello!", 6);
+    }
+
+    if( status < 0 ) perror("uh oh");
+
+    close(s);
+}
